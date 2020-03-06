a994020eace246e5d93f3d38500f56861ebb79a1
rishav <krrishav1416@gmail.com>
2020-03-06 20:17:51 +0530

    Contacts updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { FeaturesComponent } from './components/features/features.component';
 import { AboutComponent } from './components/about/about.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProductsComponent } from './components/products/products.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ProductsComponent } from './components/products/products.component';
     AboutComponent,
     LandingPageComponent,
     NavbarComponent,
-    ProductsComponent
+    TestimonialComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/testimonial/testimonial.component.spec.ts
+++ src/app/components/testimonial/testimonial.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductsComponent } from './products.component';
+import { TestimonialComponent } from './testimonial.component';
 
-describe('ProductsComponent', () => {
-  let component: ProductsComponent;
-  let fixture: ComponentFixture<ProductsComponent>;
+describe('TestimonialComponent', () => {
+  let component: TestimonialComponent;
+  let fixture: ComponentFixture<TestimonialComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ TestimonialComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductsComponent);
+    fixture = TestBed.createComponent(TestimonialComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
